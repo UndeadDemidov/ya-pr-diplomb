c9e0fb8c5f590524ac4864927770453638ea67ff
UndeadDemidov <45305584+UndeadDemidov@users.noreply.github.com>
2022-11-26 00:08:59 +0300

    fixed json naming

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,11 +11,11 @@ import (
 
 // User base model.
 type User struct {
-	UserID    uuid.UUID `json:"user_id" db:"user_id" validate:"omitempty"`
+	UserID    uuid.UUID `json:"userID" db:"user_id" validate:"omitempty"`
 	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
 	Password  string    `json:"password,omitempty" db:"password"`
-	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
+	CreatedAt time.Time `json:"createdAt,omitempty" db:"created_at"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty" db:"updated_at"`
 }
 
 // SanitizePassword removes password.
